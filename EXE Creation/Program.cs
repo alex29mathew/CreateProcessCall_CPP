dd6eae80b305411e534439fc8152a1d485f05926
alex29mathew <alex29mathew@gmail.com>
2019-09-05 14:59:27 +0530

    First commit

---
+++ EXE Creation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace EXE_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Regex cmdRegEx = new Regex(@"/(?<name>.+?)=(?<val>.+)");
+
+            Dictionary<string, string> cmdArgs = new Dictionary<string, string>();
+            foreach (string s in args)
+            {
+                Match m = cmdRegEx.Match(s);
+                if (m.Success)
+                {
+                    cmdArgs.Add(m.Groups[1].Value, m.Groups[2].Value);
+                }
+            }
+            foreach (KeyValuePair<string, string> entry in cmdArgs)
+            {
+                Console.WriteLine(entry.Key+","+ entry.Value);
+            }
+            
+
+            string root = @"C:\VTS";
+            // If directory does not exist, create it. 
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+        }
+    }
+}
